8bcf8cf1c665c7dac6ec1abf02899ab79cdf2b84
Siva Durshika <sivadurshika22@gmail.com>
2025-01-18 21:19:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,36 +16,3 @@ document.getElementById("generateMoodFact").addEventListener("click", () => {
     facts[mood][Math.floor(Math.random() * facts[mood].length)];
   document.getElementById("moodFactDisplay").innerText = randomFact;
 });
-// JavaScript for Modal Interaction
-
-// Get modal element
-var thankYouModal = document.getElementById("thankYouModal");
-
-// Get button to trigger modal
-var startBtn = document.getElementById("startBtn");
-
-// Get elements to close modal
-var closeBtn = document.getElementById("closeBtn");
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the "Start Exploring" button, show the modal
-startBtn.onclick = function() {
-    thankYouModal.style.display = "block";
-}
-
-// Close the modal when the user clicks the 'x' (span) button
-span.onclick = function() {
-    thankYouModal.style.display = "none";
-}
-
-// Close the modal when the user clicks the 'Close' button
-closeBtn.onclick = function() {
-    thankYouModal.style.display = "none";
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == thankYouModal) {
-        thankYouModal.style.display = "none";
-    }
-}
